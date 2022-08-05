af2c795b78a31be5b6d4e361dab94918e39f96d7
AbdurehmanSaleemi <asaleemi121@gmail.com>
2022-08-05 17:10:32 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import TerminalText from './TerminalText';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BsArrowRightShort } from 'react-icons/bs';
 
-function App() {
 
+function App() {
   const terminalTitle = 'Terminal';
   const terminalStart = "$ SALEEMI =>";
 
@@ -56,12 +56,12 @@ function App() {
     {
       id: 2,
       command: 'saleemi --portfolio',
-      result: "visit my portfolio: https://github.com/AbdurehmanSaleemi",
+      result: "Visit my portfolio: https://github.com/AbdurehmanSaleemi",
     },
     {
       id: 3,
       command: 'saleemi --contact',
-      result: "Contact me @asaleemi121@gmail.com. I check my emails once a month so you can be sure I'll get back to you.",
+      result: "Contact me at asaleemi121@gmail.com. I check my emails once a month so you can be sure I'll get back to you :)",
     },
     {
       id: 4,
@@ -76,7 +76,7 @@ function App() {
     {
       id: 6,
       command: 'saleemi --age',
-      result: "Age: 21",
+      result: "Maghrib se Pehla ghar wapis anay wali",
     },
     {
       id: 7,
@@ -106,51 +106,42 @@ function App() {
     setCommand(e.target.value);
   }
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    //reset everything
+    setResult('');
+    setError(false);
+    setShowHelp(false);
     setShowResult(false);
-    e.preventDefault();
+  }, [command]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (command === 'saleemi --help') {
-      setCommand('');
       setShowHelp(true);
-      setError(false);
     }
-    else if (command === 'clear') {
-      setCommand('');
-      setError(false);
-    }
-    else if (showResult == false) {
-      setCommand('');
+    else if (command !== '') {
       for (let i = 0; i < allCommands.length; i++) {
         if (command === allCommands[i].command) {
           setResult(allCommands[i].result);
           setShowResult(true);
-          setError(false);
-          setShowHelp(false);
-          break;
-        }
-        else if (i == allCommands.length - 1) {
-          setError(true);
-          setShowHelp(false);
-          setCommand('');
         }
       }
     }
   }
 
-  const disclaimer = "This project is built purely for learning purposes so the information provided here can be wrong. No part of this Application comes under any sort of free liscence. You can take inspiration from it but cannot copy its code and design."
+  const disclaimer = "This project is built purely for learning purposes so the information provided here can be wrong. No part of this Application comes under any sort of free liscence. You can take inspiration from it but cannot copy its code and design.";
 
   return (
     <div className="App">
-      <div className='h-screen w-full bg-gradient p-24 hidden lg:flex flex-auto overflow-hidden antialiased'>
-        <div className='grid grid-cols-5 gap-10 w-full h-full'>
+      <div className='h-screen w-screen bg-gradient p-24 hidden lg:flex flex-auto overflow-hidden antialiased'>
+        <div className='grid grid-cols-5 gap-12 w-full h-full'>
           <div className='col-span-1 row-span-2'>
-            <div className='flex flex-col justify-between items-start  h-full'>
-              <div className='flex flex-col justify-center items-start space-y-2 h-auto'>
+            <div className='flex flex-col justify-evenly items-start  h-full'>
+              <div className='flex flex-col justify-center items-start space-y-2 h-auto w-auto'>
                 <div><h1 className='text-green-200 text-6xl text-left font-bold'>SALEEMIX</h1></div>
                 <div><h1 className='text-green-200/30 text-[0.87rem] text-left uppercase tracking-wider font-normal'>Written in ReactJS & TailwindCSS</h1></div>
               </div>
-              <div className='flex flex-col justify-center items-start space-y-2 h-auto'>
+              <div className='flex flex-col justify-center items-start space-y-2 h-auto mt-10'>
                 <div className='mb-2 text-green-200/90 uppercase text-3xl font-bold'>disclaimer </div>
                 <div className='text-green-200/70 font-light text-justify'>{disclaimer}</div>
                 <div className='mt-7'></div>
@@ -162,22 +153,29 @@ function App() {
             <div className='flex flex-col p-2 justify-between items-center w-full'>
               <h1 className='text-green-200 tracking-wider font-bold text-5xl uppercase'>{terminalTitle}</h1>
             </div>
-            <div className='flex flex-col gap-5 justify-between items-start w-full mt-5'>
+            <div className='flex flex-col gap-5 justify-between items-start w-full mt-10'>
               <div className='flex flex-row gap-5 flex-wrap items-center w-10/12'>
                 <TerminalText handleChange={handleCommand} handleSubmit={handleSubmit} terminalStart={terminalStart} />
-                {helpShow && help.map((item, index) => { return (<h1 key={index} className='text-xl text-yellow-100 font- mr-8'>{item.help}</h1>) })}
+                <div className='flex flex-row flex-wrap items-start justify-start gap-y-5 w-full'>
+                  {helpShow && help.map((item, index) => {
+                    return (
+                      <div key={index} className='text-2xl text-yellow-100 w-1/2'>{item.help}</div>
+                    )
+                  }
+                  )}
+                </div>
                 {error && <h1 className='text-xl text-red-200 font- mr-8'>Command Not Found. Type "saleemi --help"</h1>}
-                {showResult && <h1 className='text-xl text-yellow-100 font- mr-8 leading-loose'>{result}</h1>}
+                {showResult && <h1 className='text-2xl text-yellow-100 font- mr-8 leading-loose'>{result}</h1>}
               </div>
             </div>
           </div>
           <div className='col-span-1 row-span-1 w-full h-full'>
-            <div className='flex flex-col justify-evenly h-full w-full'>
+            <div className='flex flex-col justify-around h-full w-full'>
               <div className='w-full items-center justify-center flex'>
-                <a className='w-full h-full text-center p-5 text-xl uppercase text-white btnBg ease-in duration-75 hover:scale-110 hover:ease-in hover:duration-75 cursor-pointer antialiased'>Report a Bug</a>
+                <a href='https://github.com/AbdurehmanSaleemi/react-terminal-app/issues' className='w-full h-full text-center p-5 text-xl uppercase text-white btnBg ease-in duration-75 hover:scale-110 hover:ease-in hover:duration-75 cursor-pointer antialiased'>Report a Bug</a>
               </div>
               <div className='w-full items-center justify-center flex'>
-                <a className='w-full inline-flex items-center gap-5 justify-center text-center text-xl uppercase text-white p-0 ease-in duration-75 hover:scale-110 hover:ease-in hover:duration-75 cursor-pointer antialiased'>Source Code <BsArrowRightShort className='text-2xl'/></a>
+                <a href='https://github.com/AbdurehmanSaleemi/react-terminal-app' className='w-full inline-flex items-center gap-5 justify-center text-center text-xl uppercase text-white p-0 ease-in duration-75 hover:scale-110 hover:ease-in hover:duration-75 cursor-pointer antialiased'>Source Code <BsArrowRightShort className='text-2xl' /></a>
               </div>
             </div>
           </div>
